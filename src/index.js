d42011e2025d49dec49c4d04b796091774de6a7f
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-07 21:44:01 -0500

    Get gameboard to print and move on command

--- src/index.js
+++ src/index.js
@@ -1,18 +1,80 @@
-// import "./styles/index.scss";
 import debounce from 'lodash/debounce';
 import Game from './scripts/game';
+import './styles/index.scss';
 
 
 
+const canvas = document.getElementById('game-board');
+const ctx = canvas.getContext("2d");
 
 const game = new Game(10, 3);
+let y = 0;
+
+function draw(){
+  ctx.clearRect(0, 0, 500, 500)
+  let z = y;
+  let count = 0
+  game.board.rows.forEach(row => {
+    if (z > 603) {
+     z -= 103;
+     return
+    }
+    if (z < -103) return;
+    row.drawRow(ctx, z - 103)
+    count++;
+    z -= 103;
+  })
+  console.log(count);
+ 
+  y += 12.875;
+  debugger
+  if (y % 103 !== 0) {
+    requestAnimationFrame(draw);
+  }
+
+  
+}
+window.draw = draw;
+
+
+// ctx.beginPath();
+// ctx.strokeStyle = "black";
+// ctx.strokeRect(75, 0, 350, 88);
+// ctx.strokeRect(75, 0, 70, 88);
+// ctx.strokeRect(145, 0, 70, 88);
+// ctx.strokeRect(215, 0, 70, 88);
+// ctx.strokeRect(285, 0, 70, 88);
+// ctx.strokeRect(355, 0, 70, 88);
+
+// ctx.strokeRect(75, 103, 350, 88);
+// ctx.strokeRect(75, 206, 350, 88);
+// ctx.strokeRect(75, 309, 350, 88);
+// ctx.strokeRect(75, 412, 350, 88);
+// ctx.closePath();
+// ctx.beginPath();
+// ctx.arc(109.5, 55, 11, 0, Math.PI * 2);
+// ctx.fill();
+// ctx.closePath();
+// ctx.beginPath();
+// ctx.moveTo(120, 53.5);
+// ctx.lineWidth = 2;
+// ctx.lineTo(120, 24.5);
+// ctx.stroke();
+
+
+
+
+
+
+
+
+
 
 const makeMove = keysDown => {
   keysDown = parseInt(keysDown.join(""), 2);
   if (game.checkMove(keysDown)){
     console.log("play sound");
     console.log("load next sound");
-    game.go();
   }
   else {
     console.log('play fail sound');
@@ -22,19 +84,24 @@ const makeMove = keysDown => {
 
 const debouncedMakeMove = debounce(makeMove, 80);
 
+const keyElements = document.querySelectorAll('.player-key');
 const keys = {'a': 0, 's': 1, 'd': 2, 'f': 3, 'g': 4}
 let keysDown = [0, 0, 0, 0, 0];
 
 
 document.addEventListener('keydown', (e) => {
-  if (keys[e.key] === undefined || keysDown[keys[e.key]] === 1) return;
-  keysDown[keys[e.key]] = 1;
-  debouncedMakeMove(keysDown)
+  const idx = keys[e.key]
+  if (idx === undefined || keysDown[idx] === 1) return;
+  keysDown[idx] = 1;
+  keyElements[idx].classList.add("selected");
+  debouncedMakeMove(keysDown);
 })
 
-document.addEventListener('keyup', () => { 
+document.addEventListener('keyup', (e) => { 
   keysDown = Array(5).fill(0);
+  keyElements[keys[e.key]].classList.remove("selected");
 })
+
 
 
 
--- src/scripts/move.js
+++ src/scripts/move.js
@@ -19,6 +19,36 @@ class Move {
     return row;
   }
 
+  drawRow(ctx, y){
+    ctx.beginPath();
+    ctx.strokeRect(75, y, 350, 88);
+    ctx.strokeRect(75, y, 70, 88);
+    ctx.strokeRect(145, y, 70, 88);
+    ctx.strokeRect(215, y, 70, 88);
+    ctx.strokeRect(285, y, 70, 88);
+    ctx.strokeRect(355, y, 70, 88);
+    ctx.stroke();
+    
+    this.drawMusicNotes(ctx, y);
+  }
+
+  drawMusicNotes(ctx, y){
+    this.row.forEach((slot, i) => {
+      if (slot === 1) {
+        const startX = i * 70 + 109.5;
+        const startY = y + 55
+        debugger
+        ctx.arc(startX, startY, 11, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.moveTo(startX + 10.5, startY);
+        ctx.lineTo(startX + 10.5, startY - 30);
+        ctx.stroke();
+        ctx.closePath();
+      }
+    })
+  }
+
 }
 
 export default Move;
